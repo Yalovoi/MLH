652f523ce704dd2ab3fd5ebc9ce1d8e0b8e4a3fe
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-02 22:28:32 -0800

    first commit

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,1 +1,8 @@
 const sel = require ('../../data/selectors.json');
+
+describe('Inputs are displayed', function () {
+    it('TC-014 Name ', function () {
+        browser.url('');
+        const name = $(sel.name).isDisplayed();
+        expect(name).toEqual(true);
+    });
