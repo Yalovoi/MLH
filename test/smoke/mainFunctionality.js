451e8250e5541e4de17f19a8df9ea4c7f1b9da9c
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-02 22:43:03 -0800

    first commit

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -6,8 +6,9 @@ describe('Happy path', function () {
 
     it('TC-021 Create button is clickable after 1-4 are filled in ', function () {
         browser.url('');
-
-
+        const inputName = $(sel.name).addValue("LadyBug");
+        const inputGender = $$(sel.gender)[1].click();
+        const inputAge = $(sel.age).setValue(1)
         const name = $(sel.name).isDisplayed();
         expect(name).toEqual(true);
      });
