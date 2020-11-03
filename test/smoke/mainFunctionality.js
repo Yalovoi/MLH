fc4da3c5ebd0007772db92aa5621b77698c503b7
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-02 22:31:59 -0800

    first commit

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,8 +1,13 @@
 const sel = require ('../../data/selectors.json');
 
+describe('Checking the main functionality', function () {
+
 describe('Inputs are displayed', function () {
     it('TC-014 Name ', function () {
         browser.url('');
         const name = $(sel.name).isDisplayed();
         expect(name).toEqual(true);
-    });
+     });
+  });
+
+});
